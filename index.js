6b3f82b8b7672cb02838e70f3ca3f4cc65e6ca4f
Bipin Bhandari <bhandaribpn1996@gmail.com>
2019-01-19 22:49:46 +0545

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 var app = express();
+const PORT = process.env.PORT || 5000
 
 const publicPath = path.join(__dirname,'./public');
 console.log(publicPath);
-const port = process.env.port || 3000;
+
 
 app.use(express.static(publicPath));
-app.listen(port,()=>{
-	console.log(`istening on port ${port}`);
-})
+
+app.listen(PORT);
+console.log("listening on port "+PORT);
