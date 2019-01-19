3ec74eaefa38cc8304e3acdda32ad0c551c1b551
Bipin Bhandari <bhandaribpn1996@gmail.com>
2019-01-19 23:23:39 +0545

    connect and disconnect handler

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 var app = express();
+const socketIO = require('socket.io');
 const PORT = process.env.PORT || 5000
 
 const publicPath = path.join(__dirname,'./public');
-console.log(publicPath);
-
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(PORT);
-console.log("listening on port "+PORT);
+io.on('connection',(socket)=>{
+	console.log("New User Connected");
+
+		socket.on('disconnect',()=>{
+		console.log("disconnected from server");
+	})
+})
+
+
+
+
+server.listen(PORT ,()=>{
+	console.log("listening on port "+PORT);
+
+});
